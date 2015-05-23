823463498a0e18ac187d532c8eea459e4fc0a063
Ben Kelly <bk@ancilla.ca>
2015-05-23 16:21:16 -0400

    Make ttysnap library and ttysnap package

---
+++ lib/ttysnap.lua
@@ -0,0 +1,103 @@
+local vstruct = require "vstruct"
+
+local TTY = {}
+
+function TTY:init(ttyrec, options)
+  local tty = setmetatable({}, {__index = TTY})
+  tty.file = ttyrec
+  tty.fd = assert(io.open(ttyrec, 'rb'))
+
+  -- constants
+  local s,us = vstruct.readvals('u4 u4', tty.fd)
+  tty.start = s + (us/1e6)      -- starting time of recording
+  tty.eof = tty.fd:seek('end')  -- end of file offset
+  tty.options = options
+
+  -- state
+  tty.frame = -1            -- current frame displayed in terminal
+  tty.time = 0      -- current time
+  tty.where = 0             -- read pointer offset
+
+  -- start child process
+  -- there's some ugly hacks here
+  -- first, we create a fifo named /tmp/<current time>, which is totes insecure
+  tty.fifoname = "/tmp/%d" % os.time()
+  os.execute("mkfifo '%s'" % tty.fifoname)
+
+  -- then we open an xterm in the background
+  local cmd = "xterm -T ttysnap -ah -geometry %s -fa %s -fs %d -bg black +sb -e cat '%s' &" % {
+    options.term_size,
+    options.font,
+    options.font_size,
+    tty.fifoname,
+  }
+  assert(os.execute(cmd))
+
+  -- and finally we open the fifo to it so we can send it stuff.
+  tty.xterm = assert(io.open(tty.fifoname, 'w'))
+
+  -- initialize custom colourmap
+  for id,colour in pairs(options.palette) do
+    tty.xterm:write('\x1B]%d;3;%s\x07' % {id, colour})
+  end
+
+  -- display first frame
+  tty.fd:seek('set')
+  tty:seek(0)
+  return tty
+end
+
+function TTY:close()
+  self.xterm:close()
+  os.remove(self.fifoname)
+end
+
+function TTY:rewind()
+  -- rewind to start
+  self.frame = -1
+  self.fd:seek('set')
+  self.time = 0
+  self.where = 0
+  self.xterm:write('\x1B[2J') -- CSI 2 J; Erase Display: All
+end
+
+function TTY:step()
+  -- write one frame
+  local frame = vstruct.read('s:u4 us:u4 data:c4', self.fd)
+  self.xterm:write(frame.data)
+  self.frame = self.frame + 1
+  self.time = (frame.s + frame.us/1e6) - self.start
+  self.where = self.fd:seek()
+end
+
+function TTY:seek(to_frame)
+  to_frame = math.max(to_frame, 0)
+  if self.frame > to_frame then
+    self:rewind()
+  end
+
+  while self.frame < to_frame and self.where < self.eof do
+    self:step()
+  end
+
+  self.xterm:flush()
+end
+
+function TTY:seektime(to_time)
+  to_time = math.max(to_time, 0)
+  if self.time > to_time then
+    self:rewind()
+  end
+
+  while self.time < to_time and self.where < self.eof do
+    self:step()
+  end
+
+  self.xterm:flush()
+end
+
+function TTY:snap(name)
+  return os.execute('import -window ttysnap "png:%s"' % name) and name
+end
+
+return TTY
---
+++ packages/ttysnap.lua
@@ -0,0 +1,37 @@
+local TTY = require "ttysnap"
+require "util.flags"
+require "util.io"
+require "util.string"
+
+local defaults = {
+  palette = {};
+  term_size = '80x24';
+  font = 'Cousine';
+  font_size = 16;
+  prefix = 'ttysnap/';
+}
+local options = setmetatable(..., {__index = defaults})
+
+-- parse options that need parsing
+if type(options.palette) == 'string' then
+  options.palette = flags.mapOf(flags.number, flags.string)(nil, options.palette)
+end
+options.font_size = tonumber(options.font_size)
+
+local tty
+
+-- Extract screenshots from a ttyrec file.
+-- Looks for options.prefix/<frame number>.png
+-- If it finds it, emits a normal "image" tag.
+-- If it doesn't find it, initializes TTY library if needed and then takes a
+-- screenshot.
+lp.defmacro('ttysnap', 1, function(frame)
+  frame = tonumber(frame)
+  name = '%s%08d.png' % {options.prefix, frame}
+  if not io.exists(name) then
+    if not tty then tty = TTY:init(options.file, options) end
+    tty:seek(frame)
+    tty:snap(name)
+  end
+  return '[image %s]' % name
+end)
